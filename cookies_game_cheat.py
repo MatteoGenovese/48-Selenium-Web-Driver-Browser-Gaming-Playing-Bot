33294cb0ac2f98f10e597a96c2b2155867133fa3
matteo <mgenovese@doxee.com>
2023-07-20 23:32:05 +0200

    feature: cheat that select best actor to achieve better results in terms of profit per cost unit

--- cookies_game_cheat.py
+++ cookies_game_cheat.py
@@ -1,8 +1,50 @@
 from selenium import webdriver
+from selenium.common import StaleElementReferenceException
+from selenium.webdriver.remote.webelement import WebElement
+import selenium
 from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
+
+def refreshPPU(actors):
+    for actor in actors:
+        actor["profit_per_cost_unit"] = "{:.3f}".format(actor["points_per_5_seconds"] / int(actor["cost"]))
+    return actors
+
+
+def refreshCosts(actors):
+    for actor in actors:
+        actor["cost"] = int(
+            parseWhitoutComa(driver.find_element(By.CSS_SELECTOR, f"{actor['element_css_selector']} b")))
+    refreshPPU(actors)
+    return actors
+
+def parseWhitoutComa(textToParse: WebElement) -> str:
+    if "-" in textToParse.text:
+        senzatrattini = textToParse.text.split("-")[1].strip()
+        return "".join(senzatrattini.split(","))
+    else:
+        return "".join(textToParse.text.split(","))
+
+gameActors = [
+    {"id": "buyCursor", "cost": 15, "points_per_5_seconds": 1, "profit_per_cost_unit": 0.66666,
+     "element_css_selector": "#buyCursor"},
+    {"id": "buyGrandma", "cost": 100, "points_per_5_seconds": 10, "profit_per_cost_unit": 0.1,
+     "element_css_selector": "#buyGrandma"},
+    {"id": "buyFactory", "cost": 500, "points_per_5_seconds": 20, "profit_per_cost_unit": 0.04,
+     "element_css_selector": "#buyFactory"},
+    {"id": "buyMine", "cost": 2000, "points_per_5_seconds": 50, "profit_per_cost_unit": 0.025,
+     "element_css_selector": "#buyMine"},
+    {"id": "buyShipment", "cost": 7000, "points_per_5_seconds": 100, "profit_per_cost_unit": 0.01428,
+     "element_css_selector": "#buyShipment"},
+    {"id": "buyAlchemy lab", "cost": 50000, "points_per_5_seconds": 500, "profit_per_cost_unit": 0.01,
+     "element_css_selector": "#buyAlchemy\\ lab"},
+    {"id": "portal", "cost": 1000000, "points_per_5_seconds": 6666, "profit_per_cost_unit": 0,
+     "element_css_selector": "#buyPortal"},
+    # {"id": "buyTime machine", "cost": 123456789, "points_per_5_seconds": ????, "profit_per_cost_unit": 0,
+    #  "element_css_selector": "#buyTime\\ machine"},
+]
 
 service = Service(executable_path="/home/matteo/Development/Drivers/chromedriver")
 driver = webdriver.Chrome(service=service)
@@ -23,37 +65,9 @@ alchemy_lab = driver.find_element(By.ID, "buyAlchemy lab")
 portal = driver.find_element(By.ID, "buyPortal")
 time_machine = driver.find_element(By.ID, "buyTime machine")
 
-gameActors = [
-    {"id": "buyCursor", "initial_cost": 15, "points_per_5_seconds": 1, "profit_per_cost_unit": 0.66666},
-    {"id": "buyGrandma", "initial_cost": 100, "points_per_5_seconds": 10, "profit_per_cost_unit": 0.1},
-    {"id": "buyFactory", "initial_cost": 500, "points_per_5_seconds": 20, "profit_per_cost_unit": 0.04},
-    {"id": "buyMine", "initial_cost": 2000, "points_per_5_seconds": 50, "profit_per_cost_unit": 0.025},
-    {"id": "buyShipment", "initial_cost": 7000, "points_per_5_seconds": 100, "profit_per_cost_unit": 0.01428},
-    {"id": "buyAlchemy lab", "initial_cost": 50000, "points_per_5_seconds": 500, "profit_per_cost_unit": 0.01},
-    {"id": "portal", "initial_cost": 1000000, "points_per_5_seconds": "?", "profit_per_cost_unit": 0},
-    {"id": "buyTime machine", "initial_cost": 123456789, "points_per_5_seconds": "?", "profit_per_cost_unit": 0},
-]
-
 cookiePerSecond = driver.find_element(By.ID, "cps")
 
-
-# cursor 15, 1, 3
-# grandma 100, 10, 11
-# factory 500, 20, 101
-# mine 2000, 50, 200
-# shipment 7000, 100, 700
-# alchemy lab 50000, 500, 5000
-def refreshCosts():
-    cursorCost = driver.find_element(By.CSS_SELECTOR, "buyCursor b")
-    grandmaCost = driver.find_element(By.CSS_SELECTOR, "buyGrandma b")
-    factoryCost = driver.find_element(By.CSS_SELECTOR, "buyFactory b")
-    mineCost = driver.find_element(By.CSS_SELECTOR, "buyMine b")
-    shipmentCost = driver.find_element(By.CSS_SELECTOR, "buyShipment b")
-    alchemy_labCost = driver.find_element(By.CSS_SELECTOR, "buyAlchemy\\ lab b")
-    portalCost = driver.find_element(By.CSS_SELECTOR, "buyPortal b")
-    time_machineCost = driver.find_element(By.CSS_SELECTOR, "#buyTime\\ machine b")
-    cookiePerSecondCost = driver.find_element(By.CSS_SELECTOR, "#cps b")
-
+exitForInvestmentsButton = driver.find_element(By.ID, "updateLog")
 
 cookiePerMinute = 0
 
@@ -62,29 +76,35 @@ timeUntilStart = time.time()
 scoreUntilStart = 0
 lastScore = 0
 
-flag = False
-
-# print(cursorCost)
-# print(grandmaCost)
-# print(factoryCost)
-# print(mineCost)
-# print(shipmentCost)
-# print(alchemy_labCost)
-# print(portalCost)
-# print(time_machineCost)
-# print(cookiePerSecondCost)
+flag = True
 
 while True:
-    newScore = int("".join(score.text.split(",")))
+    newScore = int(parseWhitoutComa(score))
     cookie.click()
-    if time.time() > timeout:
-        timeUntilStart += 5
+    if time.time() > timeout and flag:
         scoreUntilStart += newScore - scoreUntilStart
         timeout = time.time() + 5
         cookiePerMinute = int((scoreUntilStart - lastScore) / (5 / 60))
         lastScore = scoreUntilStart
-        print(f"timeUntilStart:{timeUntilStart}  | scoreTotal:{scoreUntilStart} | cpm:{cookiePerMinute} | {flag}")
-    if newScore > 500:
-        flag = True
+        cookiePerSecond = driver.find_element(By.ID, "cps")
+        print(f"timeUntilStart:{timeUntilStart}  | scoreTotal:{scoreUntilStart}")
+        try:
+            refreshCosts(gameActors)
+            statistics = []
+            for actor in gameActors:
+                statistics.append(str(actor["profit_per_cost_unit"]))
+            print(" | ".join(statistics))
+            print("")
+            maxNumber = 0
+            index = -1
+            indexMax = 0
+            for statistic in statistics:
+                index +=1
+                if float(statistic)> maxNumber:
+                    maxNumber = float(statistic)
+                    indexMax = index
 
+            driver.find_element(By.CSS_SELECTOR, gameActors[indexMax]["element_css_selector"]).click()
+        except StaleElementReferenceException:
+            print("StaleElementReferenceException")
     time.sleep(0.1)
